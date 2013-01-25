98c3ae3825b572a0fe22625a71660b1e8a0fd707
itzki <itzki.h@gmail.com>
2013-01-26 04:30:59 +0900

    Internationalize error messages
    Use ActiveModel::Errors instesd of flash[:error]

--- lib/issues_controller_patch.rb
+++ lib/issues_controller_patch.rb
@@ -18,9 +18,9 @@ module RedmineSpentTimeRequired
           allowed_statuses = Setting.plugin_redmine_spent_time_required['statuses'].scan(/\d+/)
           current_status = params[:issue][:status_id]
           if ((params[:time_entry][:hours] == "") && (allowed_statuses.member?(current_status.to_s)))
-            flash[:error] = "Spent time required"
             find_issue
             update_issue_from_params
+            @time_entry.errors.add :hours, :empty
             render(:action => 'edit') and return
           else
             update_without_check_spent_time
