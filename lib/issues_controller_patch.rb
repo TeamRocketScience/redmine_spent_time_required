e7f0a8180c0acd3d7a528355ede69382f52c2e25
Michel Beloshitsky <mbeloshitsky@gmail.com>
2015-04-02 01:28:59 +0500

    Proper processing when time tracking module disabled.

--- lib/issues_controller_patch.rb
+++ lib/issues_controller_patch.rb
@@ -18,7 +18,11 @@ module RedmineSpentTimeRequired
           allowed_statuses = Setting.plugin_redmine_spent_time_required['statuses'].scan(/\d+/)
           need_comment = Setting.plugin_redmine_spent_time_required['need_comment']
           current_status = params[:issue][:status_id]
-          if ((params[:time_entry][:hours] == "") && (allowed_statuses.member?(current_status.to_s)))
+          if (not (allowed_statuses.member?(current_status.to_s))) or (not params.has_key? :time_entry)
+            update_without_check_spent_time
+            return
+          end
+          if (params[:time_entry][:hours] == "")
             flash[:error] = "Spent time required"
             find_issue
             update_issue_from_params
