e5e0ff5ebd8268de8c9134caa23661871fea04e3
Michel Beloshitsky <mbeloshitsky@nilksa.ru>
2016-04-11 17:48:30 +0500

    Taking bulk updates into account

--- lib/issues_controller_patch.rb
+++ lib/issues_controller_patch.rb
@@ -6,7 +6,8 @@ module RedmineSpentTimeRequired
         base.send(:include, InstanceMethods)
         base.class_eval do
           unloadable
-          alias_method_chain :update, :check_spent_time
+          alias_method_chain :update,      :check_spent_time
+          alias_method_chain :bulk_update, :check_spent_time
         end
       end
 
@@ -14,30 +15,66 @@ module RedmineSpentTimeRequired
       end
 
       module InstanceMethods
+        
+        def check_spent_time(issue, allowed_statuses, need_comment)
+          current_status = issue.status.id
+          if params.include? :issue and params[:issue].include? :status_id
+            if not allowed_statuses.member?(params[:issue][:status_id])
+              return true
+            end
+          else
+            if not allowed_statuses.member?(current_status.to_s)
+              return true
+            end
+          end
+          spent_hours    = issue.time_entries.map { |te| te.hours }.sum 
+          if params.include? :time_entry
+            entered_hours = params[:time_entry][:hours].to_f
+          else
+            entered_hours = 0
+          end
+          if (entered_hours + spent_hours) == 0.0
+            flash[:error] = "Spent time required"
+            return false
+          end
+          if (params[:time_entry][:comments] == "") && need_comment
+            flash[:error] = "Comment required"
+            return false
+          end
+          return true
+        end
+
         def update_with_check_spent_time
-          allowed_statuses = Setting.plugin_redmine_spent_time_required['statuses'].scan(/\d+/)
-          need_comment = Setting.plugin_redmine_spent_time_required['need_comment']
-          current_status = params[:issue][:status_id]
-          if (not (allowed_statuses.member?(current_status.to_s))) or (not params.has_key? :time_entry)
-            update_without_check_spent_time
+          if not params.has_key? :time_entry
+            update_without_check_spent_time 
             return
           end
+          allowed_statuses = Setting.plugin_redmine_spent_time_required['statuses'].scan(/\d+/)
+          need_comment     = Setting.plugin_redmine_spent_time_required['need_comment']
           find_issue
-          params[:time_entry][:hours].sub! ',', '.'
-          spent_hours    = @issue.time_entries.map { |te| te.hours }.sum 
-          entered_hours  = params[:time_entry][:hours].to_f
-          if ((entered_hours + spent_hours) == 0.0)
-            flash[:error] = "Spent time required"
+          if !check_spent_time(@issue, allowed_statuses, need_comment)
             update_issue_from_params
-            render(:action => 'edit') and return
-          elsif ((entered_hours > 0.0) && (params[:time_entry][:comments] == "") && need_comment)
-            flash[:error] = "Comment required"
-            update_issue_from_params
-            render(:action => 'edit') and return
+            render(:action => 'edit')
           else
             update_without_check_spent_time
           end
         end
+
+        def bulk_update_with_check_spent_time
+          allowed_statuses = Setting.plugin_redmine_spent_time_required['statuses'].scan(/\d+/)
+          need_comment     = Setting.plugin_redmine_spent_time_required['need_comment']
+          @issues.each do |issue|
+            if !check_spent_time(issue, allowed_statuses, need_comment)
+              @saved_issues   = []
+              @unsaved_issues = @issues
+              bulk_edit
+              render(:action => 'bulk_edit') 
+              return
+            end
+          end
+          bulk_update_without_check_spent_time
+        end
+
       end
 
     end
