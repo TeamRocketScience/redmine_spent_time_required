06665f299d178bb76e3306df6edce0cb0a362cf9
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2017-05-29 11:15:42 +0300

    indentation and log.exp. fix.

--- lib/issues_controller_patch.rb
+++ lib/issues_controller_patch.rb
@@ -17,7 +17,7 @@ module RedmineSpentTimeRequired
         def update_with_check_spent_time
           allowed_statuses = Setting.plugin_redmine_spent_time_required['statuses'].scan(/\d+/)
           current_status = params[:issue][:status_id]
-          if ((params[:time_entry][:hours] == "") && (allowed_statuses.member?(current_status.to_s)))
+          if ((params[:time_entry] != nil) && (params[:time_entry][:hours] == "") && (allowed_statuses.member?(current_status.to_s)))
             flash[:error] = "Spent time required"
             find_issue
             update_issue_from_params
